6619d02ef3220a084b1e02ec25a7541c90888960
lupri95w19 <priviteraluca01@gmail.com>
2025-03-25 17:40:06 +0100

    Inizializzato header

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+<div class="w-screen h-[320px] bg-black">
+    <p>ciao</p>
+</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-center p-10">
+      <h1 class="text-3xl font-bold text-blue-600">Home Page</h1>
+      <p>Benvenuto nella home!</p>
+    </div>
+  </template>
