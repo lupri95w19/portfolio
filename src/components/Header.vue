e9f62ac51209b94f44a0c51107a3481eb002f434
lupri95w19 <priviteraluca01@gmail.com>
2025-03-26 17:13:32 +0100

    Header fatto

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,26 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref } from "vue";
 </script>
 
 <template>
-<div class="w-screen h-[320px] bg-black">
-    <p>ciao</p>
-</div>
+	<div class="bg-transparent">
+		<div class="bg-black rounded-full py-4 px-8 sticky top-0 right-0">
+			<div class="flex gap-4">
+				<div>
+					<a href="#">About</a>
+				</div>
+				<div>
+					<a href="#">Contattami</a>
+				</div>
+				<div>
+					<a href="#">I miei lavori</a>
+				</div>
+				<div>
+					<a href="#">La mia carriera</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
